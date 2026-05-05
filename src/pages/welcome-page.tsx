87799ce52319972f23f2b25ba22e3b634246958c
Vyacheslav <vyacheslav.e.201@gmail.com>
2026-05-05 19:44:25 +0300

    add content to welcome page

--- src/pages/welcome-page.tsx
+++ src/pages/welcome-page.tsx
@@ -1,3 +1,12 @@
-export const WelcomePage = () => {
-  return <div>Hi im welcome page</div>;
+const WelcomePage = () => {
+  return (
+    <div>
+      <h2>
+        Привет 👋. Это небольшое тестовое приложение задач, сделанное с
+        использованием Vite, React, React Router, Redux Toolkit, Shadcn.
+      </h2>
+    </div>
+  );
 };
+
+export { WelcomePage };
